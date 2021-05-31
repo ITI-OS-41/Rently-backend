895eeb5b483466c2ca9455a0a3e1b72dcb46310c
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-05-31 12:32:15 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const passport = require("passport")
+
+/*
+ ** ROUTES
+ */
+const users = require("./routes/users.js")
+
+/*
+ ** SETUP ENVIRONMENT
+ */
+dotenv.config()
+
+/*
+ ** MONGO DB CONNECT
+ */
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+})
+
+const app = express()
+app.use("/uploads", express.static("uploads"))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+app.use(passport.initialize())
+require("./config/passport")(passport)
+
+app.use("/api/users", users)
+
+/*
+ ** RUN APP
+ */
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`)
+})
