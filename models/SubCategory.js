13716cf62d909f46da662f67a9a65f13560f7923
Muhammed Abdurahman <muhammed.abdurahman93@gmail.com>
2021-06-13 23:13:35 +0200

    category and subcategory schemas validated

--- models/SubCategory.js
+++ models/SubCategory.js
@@ -1,27 +1,29 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-const ObjectId = require('mongoose').Types.ObjectId;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = require("mongoose").Types.ObjectId;
 
-
-const subcategorySchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    index:true
+const subcategorySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photo: String,
+    category: {
+      type: ObjectId,
+      ref: "Category",
+      required: true,
+      index: true,
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  photo: String,
-  category:{
-    type: ObjectId,
-    ref:"Category",
-    required: true,
-    index:true
-  },
-}, { timestamps: true })
+  { timestamps: true }
+);
 
-subcategorySchema.index({ rater: 1, item: 1 }, { unique: true });
+subcategorySchema.index({ name: 1, category: 1 }, { unique: true });
 
-module.exports = mongoose.model("SubCategory", subcategorySchema)
+module.exports = mongoose.model("SubCategory", subcategorySchema);
