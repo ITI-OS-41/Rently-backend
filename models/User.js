ccba779163072a13e321b0fdf47dea41be445281
nadaaskora <nadaaskora@gmail.com>
2021-06-13 22:16:53 +0200

    reset password

--- models/User.js
+++ models/User.js
@@ -1,68 +1,75 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+/** @format */
 
-const userSchema = new Schema({
-  role: {
-    type: String,
-    enum: {
-      values: ["user", "admin"],
-    },
-    default: "user"
-  },
-  photo: {
-    data: Buffer,
-    type: String,
-    // required: true
-  },
-  firstname: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  lastname: {
-    type: String,
-    trim: true,
-    required: true,
-  },
-  email: {
-    type: String,
-    lowercase: true,
-    trim: true,
-    required: true,
-    unique: true,
-  },
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  referralCode: {
-    type: String,
-  },
-  isVerified: {
-    type: Boolean,
-  },
-}, { timestamps: true })
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+	{
+		role: {
+			type: String,
+			enum: {
+				values: ['user', 'admin'],
+			},
+			default: 'user',
+		},
+		photo: {
+			data: Buffer,
+			type: String,
+			// required: true
+		},
+		firstname: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		lastname: {
+			type: String,
+			trim: true,
+			required: true,
+		},
+		email: {
+			type: String,
+			lowercase: true,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			trim: true,
+			required: true,
+			unique: true,
+		},
+		password: {
+			type: String,
+			required: true,
+		},
+		referralCode: {
+			type: String,
+		},
+		isVerified: {
+			type: Boolean,
+		},
+		resetPasswordToken: String,
+		resetPasswordExpires: Date,
+	},
+	{ timestamps: true }
+);
 
 // Duplicate the ID field.
 userSchema.virtual('id').get(function () {
-  return this._id.toHexString();
+	return this._id.toHexString();
 });
 userSchema.virtual('name').get(function () {
-  if (this.firstname || this.lastname) {
-    return this.firstname || '' + ' ' + this.lastname || '';
-  }
-  return this.username;
+	if (this.firstname || this.lastname) {
+		return this.firstname || '' + ' ' + this.lastname || '';
+	}
+	return this.username;
 });
 
 // Ensure virtual fields are serialised.
 userSchema.set('toJSON', {
-  virtuals: true,
+	virtuals: true,
 });
 
-module.exports = mongoose.model("User", userSchema)
+module.exports = mongoose.model('User', userSchema);
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,30 @@
-const router = require("express").Router()
-const User = require("../models/User")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const passport = require("passport")
+/** @format */
+
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+const { catchErrors } = require('../helpers/errors');
 
 // const validateRegisterInput = require("../validation/register")
-const validateLoginInput = require("../validation/login")
+const validateLoginInput = require('../validation/login');
 
 // Import controllers
-const { register, login } = require("../controllers/auth-controller")
+const {
+	register,
+	login,
+	forgetPassword,
+	resetPassword,
+	updatePassword,
+	confirmPassword,
+} = require('../controllers/auth-controller');
 
-router.post("/register", register)
-router.post("/login", login)
+router.post('/register', register);
+router.post('/login', login);
 
-module.exports = router
+router.post('/forget', catchErrors(forgetPassword));
+router.get('/reset/:token', catchErrors(resetPassword));
+router.post('/reset/:token', confirmPassword, updatePassword);
+
+module.exports = router;
