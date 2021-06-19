ef44adc4e2621168da5217d5686ae28b9c76add8
Mahmoud Hassan <hsn.mhmod.96@gmail.com>
2021-06-19 17:50:35 +0200

    Update Category.js
    abdo's recommendations

--- models/Category.js
+++ models/Category.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const ObjectId = require("mongoose").Types.ObjectId;
-
+const SubCategory = require("../models/SubCategory");
 const categorySchema = new Schema(
   {
     name: {
